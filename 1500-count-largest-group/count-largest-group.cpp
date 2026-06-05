339b4d424a0e2cffb3a279d79ec19ba9c79d3ec3
Utkarsh005311 <ut79852@gmail.com>
2026-06-05 21:29:34 +0530

    Time: 4 ms (12.61%) | Memory: 8 MB (80.31%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        map<int,int>mpp;
+        int s=0,d;
+        for(int i=1;i<=n;i++)
+        {   s=0;
+            int x=i;
+            while(x!=0)
+            { 
+              d=x%10;
+              s=s+d;
+              x=x/10;
+            }
+            mpp[s]+=1;
+        }
+        auto iq=mpp.begin();
+        int lg=iq->second;
+        for(auto it:mpp)
+        {
+            if(lg<it.second)
+            {
+                lg=it.second;
+            }
+        }
+        int c=0;
+        for(auto it1:mpp)
+        {
+            if(it1.second==lg)
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+};
