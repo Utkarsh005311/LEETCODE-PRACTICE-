842ad3ef27c60082c5e58860dc9acbd826153461
Utkarsh005311 <ut79852@gmail.com>
2026-06-13 00:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (84.88%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-      map<int,int>mpp;
+      /*map<int,int>mpp;
       int s=0;
       int n=nums.size();
       for(int i=0;i<n;i++)
@@ -23,7 +23,30 @@ public:
       else
       {
         return s;
+      }*/
+      int n=nums.size();
+      int s=0;
+      int hash[101]={0};
+      for(int i=0;i<n;i++)
+      {
+        hash[nums[i]]+=1;
       }
-      
+      bool k=true;
+      for(int i=0;i<n;i++)
+      {
+        if(hash[nums[i]]==1)
+        {
+            k=false;
+            s=s+nums[i];
+        }
+      }
+      if(k)
+      {
+        return 0;
+      }
+      else
+      {
+        return s;
+      }
     }
 };
