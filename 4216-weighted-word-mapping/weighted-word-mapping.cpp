92f7fdfcc215fdacf13cef8694431ff707769df4
Utkarsh005311 <ut79852@gmail.com>
2026-06-14 03:22:01 +0530

    Time: 3 ms (27.53%) | Memory: 45.4 MB (6.05%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        /* map<char,int>mpp;
-        map<int,char>mpw;
-        string ans="";
-        int s=0;
-         char ch='a';
-        for(int i=0;i<26;i++)
-        {
-            mpp[ch]=weights[i];
-            ch=char(int(ch)+1);
-        }
-        char cw='a';
-        for(int i=25;i>=0;i--)
-        {
-            mpw[i]=cw;
-            cw=char(int(cw)+1);
-        }
-        string temp="";
-        for(auto p:words)
-        {   
-            s=0;
-            temp=p;
-            for(auto ch1:temp)
-            {
-               s=s+mpp[ch1];
-            }
-            s=s%26;
-            ans=ans+mpw[s];
-        }
-        return ans;*/
         string ans="";
         for(string &word:words)
         {   int s=0;
