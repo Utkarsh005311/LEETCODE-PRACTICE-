8bbb3639c3eea849bed3ca0f8382213fcdfde2fe
Utkarsh005311 <ut79852@gmail.com>
2026-06-29 23:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (19.66%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int l=patterns.size();
         int c=0;
         bool k=false;
         for(auto s:patterns)
