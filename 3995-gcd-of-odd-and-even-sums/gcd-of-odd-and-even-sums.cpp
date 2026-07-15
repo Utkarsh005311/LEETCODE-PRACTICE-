6d738e6ba3c53467d0d30ab6f024c35aa8191216
Utkarsh005311 <ut79852@gmail.com>
2026-07-15 21:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-       int odd=n*(1+(n-1));
-       int even=n*(2+(n-1));
-       int ans=gcd(odd,even);
-       return ans;
+       return gcd(n*(1+(n-1)),n*(2+(n-1)));
     }
 };
