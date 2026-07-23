86c4f83dc108df0b3dc14e09e9da88990383fdef
Utkarsh005311 <ut79852@gmail.com>
2026-07-23 17:58:36 +0530

    Time: 3 ms (95.96%) | Memory: 19.6 MB (24.34%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-     int j = 0;
-        for (int n : nums) {
-            if (j < 2 || n != nums[j - 2]) {
-                nums[j++] = n;
+        if (nums.empty()) return 0;
+
+        int j = 0;        // Pointer to place the next valid number
+        int count = 1;    // Tracks the count of the current number
+
+        for (int i = 1; i < nums.size(); i++) {
+            // Check if the current number is a duplicate
+            if (nums[i] == nums[j]) {
+                count++;
+            } else {
+                count = 1; // Found a new number, reset count
+            }
+
+            // If the count is 2 or less, it's a valid element
+            if (count <= 2) {
+                j++;           // Move the write pointer forward
+                nums[j] = nums[i]; // Write the number
             }
         }
-        return j;
+
+        return j + 1; // Size is the last index + 1
     }
 };
